3dca5f007c1a438f16a9f6f8b46cb648fe1f6697
samar <samarmohd251@gmail.com>
2025-07-13 17:48:12 +0530

    save

--- springJDBCDemo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into alein (id,name,tech) values (101,'kalu','cpp');
-insert into alein (id,name,tech) values (102,'john','javascript');
-insert into alein (id,name,tech) values (103,'billu','python');
--- springJDBCDemo/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-create table alein(
-id int primary key,
-name varchar(50),
-tech varchar(20)
-);
